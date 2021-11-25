439c2ee71e7c8e057096831d3402eef98f6c7bfd
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2021-11-25 15:44:12 +0900

    finish

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import axios from "axios";
+import usePromise from "../lib/userPromise";
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -24,23 +25,9 @@ const sampleArtice = {
 };
 
 const NewsList = ({ category }) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        // async를 사용하는 함수 따로 선언
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=15c8832c2e314805b4ecc8795b0cd18d`);
-                setArticles(response.data.articles);
-            } catch (e) {
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=15c8832c2e314805b4ecc8795b0cd18d`);
     }, [category]);
 
     // 대기 중일 때
@@ -48,11 +35,17 @@ const NewsList = ({ category }) => {
         return <NewsListBlock>대기 중...</NewsListBlock>;
     }
     // 아직 articles 값이 설정되지 않았을 때
-    if (!articles) {
+    if (!response) {
         return null;
     }
 
-    // articles 값이 유효할 때
+    // 에러가 발생했을 때
+    if (error) {
+        return <NewsListBlock>에러 발생!</NewsListBlock>
+    }
+
+    // response 값이 유효할 때
+    const { articles } = response.data;
     return (
         <NewsListBlock>
             {articles.map(article => (
